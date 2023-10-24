5dea32e632666405e349636a3f2aa9899bb80736
ST10225560 <ST10225560@vcconnect.edu.za>
2023-10-24 12:08:35 +0200

    Added DAL Folder

---
+++ DAL/DriverDAL.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Data.SqlClient;
+using MyFleetManager.Models;
+
+namespace MyFleetManager.DAL
+{
+    public class DriverDAL
+    {
+        string connectionString;
+
+        public IEnumerable<Driver> GetAllDriver()
+        {
+            List<Driver> drivers = new List<Driver>() ;
+            using SqlConnection con = new SqlConnection(connectionString)
+        }
+    }
+}
